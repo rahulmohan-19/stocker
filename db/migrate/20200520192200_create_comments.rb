b6d00e57d173d72777d96907d6aa264ac4ecf081
Rahul Mohan <rahul.mohan@zoomcar.com>
2020-05-21 22:09:57 +0530

    migration files

---
+++ db/migrate/20200520192200_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+
+      t.integer :post_id
+      t.integer :user_id
+      t.string  :content
+
+      t.timestamps
+    end
+  end
+end
